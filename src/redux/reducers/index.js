2632650ff5115d1b75c4561d9a0d8a3a91a31fbb
khth7 <55275100+nguyenvansy123@users.noreply.github.com>
2024-04-09 15:44:19 +0700

    update code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,13 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from "./auth.reducer"
 import categoryReducer from './category.reducer';
 import articleReducer from './article.reducer';
-// import userReducer from './user.reducer';
+import userReducer from './user.reducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     category:categoryReducer,
     article:articleReducer,
-    // user:userReducer
+    user:userReducer
 })
 
 export default rootReducer
